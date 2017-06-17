0bcb5fb79a62a4728bb404247a492da9e9189e71
Suo Qin <kpsesuo@gmail.com>
2017-06-17 19:54:49 +1000

    fetch real data

--- app/src/main/java/com/example/qsuo/kotlinapp/features/news/NewsManager.kt
+++ app/src/main/java/com/example/qsuo/kotlinapp/features/news/NewsManager.kt
@@ -1,25 +1,27 @@
 package com.example.qsuo.kotlinapp.features.news
 
+import com.example.qsuo.kotlinapp.api.RestAPI
 import com.example.qsuo.kotlinapp.commons.RedditNewsItem
 import rx.Observable
 
-class NewsManager {
-    fun getNews(): Observable<List<RedditNewsItem>> {
-        return Observable.create {
-            subscriber ->
-            val news = mutableListOf<RedditNewsItem>()
-            for (i in 1..10) {
-                news.add(RedditNewsItem(
-                        "author$i",
-                        "Title $i",
-                        i,
-                        1457207701L - i * 2000,
-                        "http://lorempixel.com/200/200/technics/$i",
-                        "url"
-                ))
-            }
-            subscriber.onNext(news)
-            subscriber.onCompleted()
+class NewsManager(private val api: RestAPI = RestAPI()) {
+  fun getNews(): Observable<List<RedditNewsItem>> {
+    return Observable.create {
+      subscriber ->
+      val callResponse = api.getNews("", "10")
+      val response = callResponse.execute()
+      if (response.isSuccessful) {
+        val news = response.body().data.children.map {
+          val item = it.data
+          RedditNewsItem(item.author, item.title, item.num_comments, item
+            .created, item.thumbnail, item.url)
         }
+        subscriber.onNext(news)
+        subscriber.onCompleted()
+      } else {
+        subscriber.onError(Throwable(response.message()))
+      }
+
     }
+  }
 }
