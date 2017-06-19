b3876bf85800c652eeea7b1c6eff2eb107895390
Suo Qin <kpsesuo@gmail.com>
2017-06-19 19:35:43 +1000

    make class parcelable

--- app/src/main/java/com/example/qsuo/kotlinapp/commons/Extensions.kt
+++ app/src/main/java/com/example/qsuo/kotlinapp/commons/Extensions.kt
@@ -1,5 +1,7 @@
 package com.example.qsuo.kotlinapp.commons
 
+import android.os.Parcel
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,13 +10,21 @@ import com.example.qsuo.kotlinapp.R
 import com.squareup.picasso.Picasso
 
 fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false): View {
-    return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
+  return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
 }
 
 fun ImageView.loadImg(imageUrl: String) {
-    if(imageUrl.isEmpty()) {
-        Picasso.with(context).load(R.mipmap.ic_launcher).into(this)
-    } else {
-        Picasso.with(context).load(imageUrl).into(this)
-    }
+  if (imageUrl.isEmpty()) {
+    Picasso.with(context).load(R.mipmap.ic_launcher).into(this)
+  } else {
+    Picasso.with(context).load(imageUrl).into(this)
+  }
 }
+
+inline fun <reified T: Parcelable> createParcel(
+  crossinline createFromParcel: (Parcel) -> T?): Parcelable.Creator<T> =
+  object : Parcelable.Creator<T> {
+    override fun createFromParcel(source: Parcel?): T = createFromParcel(source)
+
+    override fun newArray(size: Int): Array<out T?>  = arrayOfNulls(size)
+  }
--- app/src/main/java/com/example/qsuo/kotlinapp/commons/Models.kt
+++ app/src/main/java/com/example/qsuo/kotlinapp/commons/Models.kt
@@ -1,13 +1,38 @@
 package com.example.qsuo.kotlinapp.commons
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.example.qsuo.kotlinapp.commons.adapter.LocalViewTypes.NEWS
 import com.example.qsuo.kotlinapp.commons.adapter.ViewType
+import com.example.qsuo.kotlinapp.commons.createParcel
 
 data class RedditNews(
   val after: String,
   val before: String,
   val news: List<RedditNewsItem>
-)
+): Parcelable {
+
+  companion object {
+    val CREATOR = createParcel {RedditNews(it)}
+  }
+
+  protected constructor(parcelIn: Parcel): this(
+    parcelIn.readString(),
+    parcelIn.readString(),
+    mutableListOf<RedditNewsItem>().apply {
+      parcelIn.readTypedList(this, RedditNewsItem.CREATOR)
+    }
+  )
+
+  override fun writeToParcel(dest: Parcel, flags: Int) {
+    dest.writeString(after)
+    dest.writeString(before)
+    dest.writeTypedList(news)
+  }
+
+  override fun describeContents() = 0
+
+}
 
 data class RedditNewsItem(
   val author: String,
@@ -16,6 +41,31 @@ data class RedditNewsItem(
   val created: Long,
   val thumbnail: String,
   val url: String
-) : ViewType {
+) : ViewType, Parcelable {
+  companion object {
+    val CREATOR = createParcel { RedditNewsItem(it) }
+  }
+
+  protected constructor(parcelIn: Parcel) : this(
+    parcelIn.readString(),
+    parcelIn.readString(),
+    parcelIn.readInt(),
+    parcelIn.readLong(),
+    parcelIn.readString(),
+    parcelIn.readString()
+  )
+
+
+  override fun writeToParcel(dest: Parcel, flags: Int) {
+    dest.writeString(author)
+    dest.writeString(title)
+    dest.writeInt(numberComments)
+    dest.writeLong(created)
+    dest.writeString(thumbnail)
+    dest.writeString(url)
+  }
+
+  override fun describeContents() = 0
+
   override fun getViewType(): Int = NEWS
 }
